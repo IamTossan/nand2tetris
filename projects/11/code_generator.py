8c399c2c750131a454a1e6037d0e169584eb19cd
tossan <tossanmusic@gmail.com>
2023-02-04 22:26:26 +0100

    feat: add local var table

--- projects/11/code_generator.py
+++ projects/11/code_generator.py
@@ -7,8 +7,8 @@ class CodeGenerator:
     def __init__(self, file_name, tokens):
         self.file_name = file_name
         self.tokens = tokens
-        self.static = []
-        self.field = []
+        self.static = {}
+        self.field = {}
 
     def write(self, ls):
         with open(f"dist/{self.file_name}.vm", "w") as f:
@@ -27,7 +27,7 @@ class CodeGenerator:
     def set_identifier(self, name, kind):
         pass
 
-    def compile_class_vars(self, ls):
+    def set_class_vars(self, ls):
         ls.pop(0)
         kind = ls.pop(0)["label"]
         _type = ls.pop(0)["label"]
@@ -38,18 +38,74 @@ class CodeGenerator:
                     "type": _type,
                     "index": len(self.static),
                 }
-                self.static.append(v)
+                self.static[v['name']] = v
             else:
                 v = {
                     "name": ls.pop(0)["label"],
                     "type": _type,
                     "index": len(self.field),
                 }
-                self.field.append(v)
+                self.field[v['name']] = v
             if ls[0]["label"] == ",":
                 ls.pop(0)
         ls.pop(0)
         ls.pop(0)
+
+    def get_args(self, ls):
+        args = {}
+        ls.pop(0)
+        while ls[0]['kind'] != 'parameterList' and ls[0]['type'] != 'close':
+            _type = ls.pop(0)["label"]
+            name = ls.pop(0)["label"]
+            args[name] = {
+                "name": name,
+                "type": _type,
+                "index": len(args),
+            }
+            if ls[0]['kind'] != 'parameterList':
+                ls.pop(0)
+        ls.pop(0)
+        return args
+
+    def get_local(self, ls):
+        local = {}
+        while ls[0]['kind'] == 'varDec' and ls[0]['type'] == 'open':
+            ls.pop(0)
+            ls.pop(0)
+            _type = ls.pop(0)['label']
+            while ls[0]['kind'] == 'identifier':
+                name = ls.pop(0)['label']
+                local[name] = {
+                    "name": name,
+                    "type": _type,
+                    "index": len(local),
+                }
+                ls.pop(0)
+            ls.pop(0)
+        return local
+
+    def compile_subroutine_body(self, ls, args):
+        ls.pop(0)
+        ls.pop(0)
+        while ls[0]['kind'] != 'subroutineBody':
+            if ls[0]['kind'] == 'varDec':
+                local = self.get_local(ls)
+            else:
+                ls.pop(0)
+        print(args)
+
+    def compile_subroutine(self, ls):
+        ls.pop(0)
+        kind = ls.pop(0)['label']
+        return_type = ls.pop(0)['label']
+        subroutine_name = ls.pop(0)['label']
+
+        ls.pop(0)
+        args = self.get_args(ls)
+        ls.pop(0)
+
+        self.compile_subroutine_body(ls, args)
+
 
     def compile(self, ls):
         pprint.pprint(ls[:20])
@@ -58,7 +114,9 @@ class CodeGenerator:
         self.class_name = ls.pop(0)["label"]
         ls.pop(0)
         while ls[0]["kind"] == "classVarDec":
-            self.compile_class_vars(ls)
+            self.set_class_vars(ls)
+        while ls[0]["kind"] == "subroutineDec":
+            self.compile_subroutine(ls)
 
         pprint.pprint(ls[:10])
 
